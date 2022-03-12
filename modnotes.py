3b2c3cf35586077aa98db69de9114cf54ab9f080
Tsu-HaoLiu <96331813+Tsu-HaoLiu@users.noreply.github.com>
2022-03-11 17:38:53 -0800

    update modnotes.py

--- modnotes.py
+++ modnotes.py
@@ -137,8 +137,6 @@ def process_notes(sub_id, full_notes, notes):
 def safe_checks():
     """Checks if the subreddit entered is valid and that you moderate it"""
 
-    if not subreddit or " " in subreddit: raise SystemExit("NameError: Subreddit name entered incorrectly")
-    
     try:
         sub = r.subreddit(subreddit)
         mod_list = sub.moderator()
@@ -164,6 +162,6 @@ if __name__ == '__main__':
         raise SystemExit(f"TypeError: {sys.argv[0]} takes one argument ({len(sys.argv)-1} given)")
     user_input = sys.argv[1].strip()
     if not re.match('^[\/:A-Za-z0-9_]+$', user_input):
-        raise SystemExit(f"NameError: {user_input} does not look like a valid subreddit")
+        raise SystemExit(f"NameError: [{user_input}] does not look like a valid subreddit")
     subreddit = re.sub("/?r/", "", sys.argv[1])
     safe_checks()
