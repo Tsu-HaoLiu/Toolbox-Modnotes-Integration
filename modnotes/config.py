1ab9e32163558038b113b3f4bf1a8d05b66eb7a6
Tsu-Hao Liu <96331813+Tsu-HaoLiu@users.noreply.github.com>
2022-03-21 02:03:10 -0700

    Update config.py

--- modnotes/config.py
+++ modnotes/config.py
@@ -3,9 +3,9 @@ import configparser
 
 
 praw_file = "praw.ini"
+config = configparser.ConfigParser()
 
 def save_praw(ci, cs, u, p, s):
-    config = configparser.ConfigParser()
     config_var = {
             "client_id": ci,
             "client_secret": cs,
@@ -25,6 +25,18 @@ def save_praw(ci, cs, u, p, s):
     config.write(open(praw_file, 'w'))
     
     return
+
+
+def retrive_ini():
+    if os.path.exists(praw_file):
+        config.read(praw_file)
+        ini_dict = {s:dict(config.items(s)) for s in config.sections()}
+        if ":" in ini_dict.get('password', ""):
+            ini_dict['password'] = ini_dict['password'].split(":")[0]
+        ini_dict.pop('user_agent', None)
+        ini_list = [*ini_dict.values()]
+        return ini_list
+    return [""] * 5
 
 
 def burn_everything():
