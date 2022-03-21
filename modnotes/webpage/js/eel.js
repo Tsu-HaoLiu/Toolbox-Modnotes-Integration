4b3f29d06044439c58f6ef6c06eeab346e695264
Tsu-Hao Liu <96331813+Tsu-HaoLiu@users.noreply.github.com>
2022-03-21 01:50:11 -0700

    Add files via upload

---
+++ modnotes/webpage/js/eel.js
@@ -0,0 +1,50 @@
+// This js specificity communicates with the exposed python backend.
+
+function eelTotalNotes(t) {totalNotes(t);}
+
+function eelActionedNotes(t) {actionedNotes(t);}
+
+function eelCoinsLeft(c) {coinsleft(c);}
+
+function pStartNoteProcess() {eel.startNotes();}
+
+function pRetrieveCSV() {eel.downloadCSV();}
+
+function pRetrieveJSON() {eel.downloadJSON();}
+
+function eelFillForm(l) {fillForm(l);}
+
+function eelupdateBtn() {btnSwitcher();}
+
+
+// print to textarea
+function printjs(txt) {
+    const text = txt.toString();
+    var textarea = frames['output-display'];
+    textarea.innerHTML += text
+    if (!text.endsWith('\n')) {
+        textarea.innerHTML += '\n'; // If there was no new line, add one
+    }
+    textarea.scrollBy(0, textarea.scrollHeight);
+}
+
+
+// send validated data to python for authentication
+function sendValidatedData(filledForm, formID) {
+    if (filledForm.length == formID.length) {
+        eel.authentication(filledForm);
+    }
+}
+
+// expose functions for python
+function eelmain() {
+    eel.expose(printjs);
+    eel.expose(eelupdateBtn);
+    eel.expose(eelCoinsLeft);
+    eel.expose(eelTotalNotes);
+    eel.expose(eelActionedNotes);
+    eelFillForm(eel.browser__init());
+}
+
+
+window.addEventListener('DOMContentLoaded', () => eelmain(), false);
