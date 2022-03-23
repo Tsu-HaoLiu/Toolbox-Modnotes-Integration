7f738cd09ceb3731e308aad21c92da1832af376c
Tsu-HaoLiu <96331813+Tsu-HaoLiu@users.noreply.github.com>
2022-03-23 01:45:11 -0700

    main commit

--- modnotes/config.py
+++ modnotes/config.py
@@ -2,6 +2,7 @@ import os
 import re
 import configparser
 
+"""Save, retrive and delete praw.ini data"""
 
 praw_file = "praw.ini"
 ini_keys = ["client_id", "client_secret", "username", "password", "subreddit"]
