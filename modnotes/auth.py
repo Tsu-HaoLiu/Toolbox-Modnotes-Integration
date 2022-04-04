b1b70499f8722d4cef1e2b1b19dc88f9f9c9cb4a
Tsu-Hao Liu <96331813+Tsu-HaoLiu@users.noreply.github.com>
2022-04-04 00:47:07 -0700

    Update auth.py

--- modnotes/auth.py
+++ modnotes/auth.py
@@ -56,19 +56,20 @@ def auth(auth_details: list = None):
 
     if os.path.exists(praw_file):
         try:
-            r = praw.Reddit('DEFAULT',     
-                            check_for_updates=False,
-                            comment_kind="t1",
-                            message_kind="t4",
-                            redditor_kind="t2",
-                            submission_kind="t3",
-                            subreddit_kind="t5",
-                            trophy_kind="t6",
-                            oauth_url="https://oauth.reddit.com",
-                            reddit_url="https://www.reddit.com",
-                            short_url="https://redd.it",
-                            ratelimit_seconds=5,
-                            timeout=16
+            r = praw.Reddit(
+                'DEFAULT',
+                check_for_updates=False,
+                comment_kind="t1",
+                message_kind="t4",
+                redditor_kind="t2",
+                submission_kind="t3",
+                subreddit_kind="t5",
+                trophy_kind="t6",
+                oauth_url="https://oauth.reddit.com",
+                reddit_url="https://www.reddit.com",
+                short_url="https://redd.it",
+                ratelimit_seconds=5,
+                timeout=16
             )
             logger.warning(f"[praw.ini] Successfully logged in as u/{r.user.me().name}")
             
